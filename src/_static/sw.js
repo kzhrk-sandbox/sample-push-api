0fa3fc3455b8cd7bf8d10012be341854be3faf27
kzhrk <info@kzhrk.com>
2016-05-27 09:54:45 +0900

    update

--- src/_static/sw.js
+++ src/_static/sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('push', function(event) {
-  console.log('Received a push message', event);
-
-  var title = 'Sample Push API';
-  var body = 'We have received a push message.';
-  var icon = 'http://placekitten.com/192/192';
-
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon
-    })
-  );
+  // console.log('Received a push message', event);
+  //
+  // var title = 'Sample Push API';
+  // var body = 'We have received a push message.';
+  // var icon = 'http://placekitten.com/192/192';
+  //
+  // event.waitUntil(
+  //   self.registration.showNotification(title, {
+  //     body: body,
+  //     icon: icon
+  //   })
+  // );
 });
